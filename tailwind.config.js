ab69486f7d5c3d903b17b2e5a94c7d6425f766f9
Jennovator <139597511+Jennovator@users.noreply.github.com>
2023-11-05 04:02:00 -0500

    #Update: added themes for design

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,16 +14,29 @@ module.exports = {
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
+        "fuschia": "#915eff",
+        "dark": "#050816"
       },
       boxShadow: {
         card: "0px 35px 120px -15px #211e35",
       },
       screens: {
+        xxs: "390px",
         xs: "450px",
+        sm: "640px",
+        md: "768px",
+        lg: "960px",
+        xl: "1200px"
       },
       backgroundImage: {
         "hero-pattern": "url('/src/assets/hero.png')",
+        "home-pattern": "url('/src/assets/herobg.png')"
       },
+      container: {
+        padding: {
+          DEFAULT: "15px"
+        }
+      }
     },
   },
   plugins: [],
