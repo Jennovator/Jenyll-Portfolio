2f2baf16fb4d690230dde080020cf816eae3f10c
Jennovator <139597511+Jennovator@users.noreply.github.com>
2023-11-05 04:08:31 -0500

    #Update: updated structure for the app and change bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import { BrowserRouter } from "react-router-dom";
-import {  Hero, NavBar, About, Tech, Projects, Testimonials, Contact } from './components';
+import {  Hero, NavBar, Header, About, Tech, Projects, Testimonials, Contact } from './components';
 
 const App = () => {
   return (
       <BrowserRouter>
-        <div className="relative z-0 bg-primary">
-          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-            <NavBar />
+        <div className="relative z-0 bg-dark h-[4000px]">
+          <div className="bg-home-pattern bg-cover bg-no-repeat bg-center h-[950px]">
+            <Header />
             <Hero />
+            <NavBar />
           </div>
           <About />
           <Tech />
           <Projects />
           <Testimonials/>
-        
-          <div className="relative z-0">
-            <Contact />
-          </div>
+          <Contact />
         </div>
       </BrowserRouter>
   )
 }
 
 export default App;
+
+
