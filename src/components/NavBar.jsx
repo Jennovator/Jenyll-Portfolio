66e840fc66270534e05393956783922ca7553444
Jennovator <139597511+Jennovator@users.noreply.github.com>
2023-11-07 16:59:53 -0500

    #Update: updated offset

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,7 +13,7 @@ const NavBar = () => {
           variants={slideIn('down', 'tween', 0.5, 1)}
           initial="hidden"
           animate="show"
-          viewport={{once: false, amount: 0.7}}
+          viewport={{once: false, amount: 0.8}}
           className='w-full bg-black-100 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] 
           mx-auto px-5 flex justify-between items-center text-2xl text-secondary'
         >
@@ -26,7 +26,7 @@ const NavBar = () => {
                 activeClass='active'
                 smooth={true}
                 spy={true}
-                offset={-200}
+                offset={-150}
                 duration={800}
                 className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center hover:bg-[#915eff] hover:text-black hover:rounded-3xl'
               > {/* Add a key to the parent div */}
