9ae0b04b328bf294697ddafb7fdcff3bc5cbc230
Jennovator <139597511+Jennovator@users.noreply.github.com>
2023-11-07 17:05:14 -0500

    #Update: added new imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Hero from "./Hero";
 import NavBar from "./NavBar";
+import Header from "./Header";
 import About from "./About";
 import Tech from "./Tech";
 import Projects from "./Projects";
@@ -7,6 +8,7 @@ import Testimonials from "./Testimonials";
 import Contact from "./Contact";
 
 export {
+  Header,
   Hero,
   NavBar,
   About,
