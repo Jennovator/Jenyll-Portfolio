2dc7f04c295615895ebe75f41b5c49c0b947607b
Jennovator <139597511+Jennovator@users.noreply.github.com>
2023-11-07 17:13:04 -0500

    #Update: removed background height

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {  Hero, NavBar, Header, About, Tech, Projects, Testimonials, Contact } f
 const App = () => {
   return (
       <BrowserRouter>
-        <div className="relative z-0 bg-dark h-[4000px]">
+        <div className="relative z-0 bg-dark">
           <div className="bg-home-pattern bg-cover bg-no-repeat bg-center h-[950px]">
             <Header />
             <Hero />
