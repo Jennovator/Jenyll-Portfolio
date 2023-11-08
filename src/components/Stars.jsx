ee7ad59be15ac40eef8e73eab932b87eba769f06
Jennovator <139597511+Jennovator@users.noreply.github.com>
2023-11-08 16:11:25 -0500

    #Update: manually define and generate positions for the sphere

--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -6,34 +6,52 @@ import * as random from 'maath/random/dist/maath-random.esm';
 const Stars = (props) => {
     const ref = useRef();
 
-    const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 })
+    // Define the number of spheres you want to create
+    const numSpheres = 3;
+    const positions = new Float32Array(numSpheres * 1 * 3000);
 
-    useFrame((state, delta) =>{
+    // Generate positions for multiple spheres and concatenate them into the positions array
+    for (let i = 0; i < numSpheres; i++) {
+        const offset = i * 1 * 3000;
+        const sphere = random.inSphere(new Float32Array(3000), { radius: 30 });
+
+        // Copy the positions of the current sphere into the positions array
+        positions.set(sphere, offset);
+    }
+
+    // Check for NaN values in the positions array
+    if (positions.some(isNaN)) {
+        console.error('Generated positions contain NaN values:', positions);
+        return null; // Render nothing if NaN values are detected
+    }
+
+    useFrame((state, delta) => {
         ref.current.rotation.x -= delta / 10;
         ref.current.rotation.y -= delta / 15;
-    })
+    });
 
     return (
-        <group rotation={[ 0, 0, Math.PI / 4]}>
-            <Points 
-                ref={ref} 
-                positions={sphere}
-                stride={3} 
+        <group rotation={[0, 0, Math.PI / 4]}>
+            <Points
+                ref={ref}
+                positions={positions}
+                stride={3}
                 frustumCulled
                 {...props}
             >
                 <PointMaterial
                     transparent
                     color="#f272c8"
-                    size={0.002}
+                    size={0.1} // Adjust size for visibility in the scene
                     sizeAttenuation={true}
                     depthWrite={false}
                 />
             </Points>
         </group>
+    );
+};
 
-    )
-}
+
 
 const StarsCanvas = () => {
     return (
