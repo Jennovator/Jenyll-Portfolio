eca1adf9bf29f1de0aa02be129f5389f7eb15d7b
Jennovator <jenylltrinidadmabborang@gmail.com>
2023-11-08 21:57:29 -0500

    #Update: root app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import { BrowserRouter } from "react-router-dom";
-import {  Hero, NavBar, Header, About, Projects, Testimonials, Contact, StarsCanvas } from './components';
+import { Hero, NavBar, Header, About, Projects, Testimonials, Contact, StarsCanvas } from './components';
 
 const App = () => {
   return (
-      <BrowserRouter>
-        <div className="relative z-0 bg-dark">
-          <div className="bg-home-pattern bg-cover bg-no-repeat bg-center h-[950px]">
-            <Header />
-            <Hero />
-            <NavBar />
-          </div>
-          <About />
-          <Projects />
-          <Testimonials/>
+    <BrowserRouter>
+      <div className="relative z-0 bg-dark">
+        <div className="bg-home-pattern bg-cover bg-no-repeat bg-center h-[950px]">
+          <Header />
+          <Hero />
+          <NavBar />
+        </div>
+        <About />
+        <Projects />
+        <Testimonials />
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
         </div>
-          
-        </div>
-      </BrowserRouter>
+
+      </div>
+    </BrowserRouter>
   )
 }
 
